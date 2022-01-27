b23637c3ca7235bb99ee1154c1fc82b99f8c42b1
Tufahel <tufahel96@gmail.com>
2022-01-28 00:46:04 +0600

    contact validation with data save in local storage and auto fill

--- script.js
+++ script.js
@@ -205,7 +205,26 @@ const getFormData = () => {
 };
 
 saveButton.onclick = (event) => {
-  event.preventDefault();
+  const form = document.getElementById('form');
+  function validation(e) {
+    const email = document.getElementById('email').value;
+
+    const text = document.getElementById('text');
+    const pattern = /^([a-z0-9._]+)@([a-z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+    if (email.match(pattern)) {
+      form.classList.add('valid');
+      form.classList.remove('invalid');
+      text.innerHTML = 'Your Email Address is valid';
+      text.style.color = 'green';
+    } else {
+      text.innerHTML = 'Enter valid Email using small letter only';
+      text.style.color = 'red';
+      e.preventDefault();
+    }
+  }
+
+  form.addEventListener('submit', validation);
   const data = getFormData();
   localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
 };
