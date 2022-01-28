f613c2a1882afa4b3fae901ff1d73f1185b73cc7
Tufahel <tufahel96@gmail.com>
2022-01-28 21:19:47 +0600

    fix bug

--- script.js
+++ script.js
@@ -218,6 +218,7 @@ saveButton.onclick = (event) => {
       text.innerHTML = 'Your Email Address is valid';
       text.style.color = 'green';
     } else {
+      text.innerHTML = 'Enter valid Email using small letter only';
       text.style.color = 'red';
       e.preventDefault();
     }
