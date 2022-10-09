a2df15af487c7eef490e4b03c42972bc55fed54b
Tufahel <tufahel96@gmail.com>
2022-10-09 14:47:47 +0600

    navigation refactor

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+
+closeIcon.style.display = 'none';
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    closeIcon.style.color = 'gray';
+    hamburger.style.background = 'none';
+    menuIcon.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuItems.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
+
 const projects = [
   {
     title: 'Tonic',
